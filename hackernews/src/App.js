f6045ee08d80e0a39c221a4b9320058379b04180
Andrew Garner <andrewgarner101@gmail.com>
2018-09-18 07:14:54 -0700

    Ch1 checkpoint

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        const person = {
+          firstName: "Some",
+          lastName: "User"
+        };
+        const helloWorld = `Hello, ${person.firstName} ${person.lastName}`;
+        return (
+            <div className="App">
+                <h2>{helloWorld}</h2>
+            </div>
+        );
+    }
 }
 
 export default App;
